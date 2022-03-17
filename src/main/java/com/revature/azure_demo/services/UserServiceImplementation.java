afdcab9bb58deaebfa52ba12e2cc159e232c2e91
John-Salguero <John.Salguero@Revature.net>
2022-03-17 13:00:03 -0700

    details details... sigh - I think it should work now

--- src/main/java/com/revature/azure_demo/services/UserServiceImplementation.java
+++ src/main/java/com/revature/azure_demo/services/UserServiceImplementation.java
@@ -3,10 +3,12 @@ package com.revature.azure_demo.services;
 import com.revature.azure_demo.models.User;
 import com.revature.azure_demo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class UserServiceImplementation implements UserService{
     @Autowired
     UserRepository userRepository;
